598272a7f7122b7fb4cfbff5caef2378878e1730
SmalkiGroove <chapeau.g77@gmail.com>
2020-12-09 20:31:03 +0100

    Field engi builder

--- MCP/hook/lua/AI/AIAddBuilderTable.lua
+++ MCP/hook/lua/AI/AIAddBuilderTable.lua
@@ -3,6 +3,7 @@ function AddGlobalBaseTemplate(aiBrain, locationType, baseBuilderName)
     -- Don't use this with AI-Uveso
     if not aiBrain.Uveso then
         SPEW('MCP: Injecting BuilderGroups')
+        AddGlobalBuilderGroup(aiBrain, locationType, 'FieldEngineer Builder')
         AddGlobalBuilderGroup(aiBrain, locationType, 'TECHEXP Builder')
         AddGlobalBuilderGroup(aiBrain, locationType, 'FACTORYEXP Builder')
         AddGlobalBuilderGroup(aiBrain, locationType, 'HeavyDefenses')
---
+++ MCP/lua/AI/AIBuilders/FieldEngiBuilders.lua
@@ -0,0 +1,150 @@
+
+-- Add locals for BuilderConditions
+local UCBC = '/lua/editor/UnitCountBuildConditions.lua'
+local EBC = '/lua/editor/EconomyBuildConditions.lua'
+
+BuilderGroup {
+    BuilderGroupName = 'FieldEngineer Builder',
+    BuildersType = 'FactoryBuilder',
+    # ============
+    #    TECH 1
+    # ============
+    Builder {
+        BuilderName = 'T1 Field Engineer Disband - Init',
+        PlatoonTemplate = 'T1BuildFieldEngineer',
+        Priority = 900,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 4, 'ENGINEER TECH1' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 3, 'FIELDENGINEER TECH1' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH1' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech1' } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T1 Field Engineer Disband - Filler 1',
+        PlatoonTemplate = 'T1BuildFieldEngineer',
+        Priority = 800,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 8, 'ENGINEER TECH1' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 7, 'FIELDENGINEER TECH1' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH1' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech1' } },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T1 Field Engineer Disband - Filler 2',
+        PlatoonTemplate = 'T1BuildFieldEngineer',
+        Priority = 700,
+        BuilderConditions = {
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0.9, 1.1} },
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH1' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech1' } },
+            { IBC, 'BrainNotLowMassMode', {} },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+
+    # ============
+    #    TECH 2
+    # ============
+    Builder {
+        BuilderName = 'T2 Field Engineer Disband - Init',
+        PlatoonTemplate = 'T2BuildFieldEngineer',
+        Priority = 925,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 2, 'ENGINEER TECH2' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 3, 'FIELDENGINEER TECH2' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH2' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech2' } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T2 Field Engineer Disband - Filler 1',
+        PlatoonTemplate = 'T2BuildFieldEngineer',
+        Priority = 800,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 4, 'ENGINEER TECH2' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 6, 'FIELDENGINEER TECH2' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH2' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech2' } },
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0.95, 1.2 } },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T2 Field Engineer Disband - Filler 2',
+        PlatoonTemplate = 'T2BuildFieldEngineer',
+        Priority = 700,
+        BuilderConditions = {
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0.95, 1.2} },
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH2' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech2' } },
+            { IBC, 'BrainNotLowMassMode', {} },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+
+    # ============
+    #    TECH 3
+    # ============
+    Builder {
+        BuilderName = 'T3 Field Engineer Disband - Init',
+        PlatoonTemplate = 'T3BuildFieldEngineer',
+        Priority = 950,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 2, 'ENGINEER TECH3' }},
+            { UCBC,'EngineerLessAtLocation', { 'LocationType', 6, 'FIELDENGINEER TECH3' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH3' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech3' } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T3 Field Engineer Disband - Filler',
+        PlatoonTemplate = 'T3BuildFieldEngineer',
+        Priority = 900,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 4, 'ENGINEER TECH3' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 9, 'FIELDENGINEER TECH3' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH3' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech3' } },
+            #{ UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T3 Field Engineer Disband - Filler 2',
+        PlatoonTemplate = 'T3BuildFieldEngineer',
+        Priority = 800,
+        BuilderConditions = {
+            { UCBC, 'EngineerGreaterAtLocation', { 'LocationType', 6, 'ENGINEER TECH3' }},
+            { UCBC, 'EngineerLessAtLocation', { 'LocationType', 12, 'FIELDENGINEER TECH3' }},
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH3' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech3' } },
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0.95, 1.2 } },
+            { IBC, 'BrainNotLowMassMode', {} },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+    Builder {
+        BuilderName = 'T3 Field Engineer Disband - Filler 3',
+        PlatoonTemplate = 'T3BuildFieldEngineer',
+        Priority = 700,
+        BuilderConditions = {
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0.95, 1.2 } },
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'FIELDENGINEER TECH3' } },
+            { UCBC, 'EngineerCapCheck', { 'LocationType', 'Tech3' } },
+            { IBC, 'BrainNotLowMassMode', {} },
+            { UCBC, 'UnitCapCheckLess', { 0.8 } },
+        },
+        BuilderType = 'All',
+    },
+}
