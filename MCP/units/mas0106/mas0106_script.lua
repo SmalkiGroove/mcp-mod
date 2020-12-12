f1abcd759a9f0dc47fdd0a7d467485ae753bcc1c
SmalkiGroove <chapeau.g77@gmail.com>
2020-12-12 23:32:51 +0100

    Better T1 Arti ships

--- MCP/units/mas0106/mas0106_script.lua
+++ MCP/units/mas0106/mas0106_script.lua
@@ -1,30 +1,19 @@
 local ASeaUnit = import('/lua/aeonunits.lua').ASeaUnit
 local AeonWeapons = import('/lua/aeonweapons.lua')
-local AIFArtilleryMiasmaShellWeapon = AeonWeapons.AIFArtilleryMiasmaShellWeapon
+local AIFArtilleryMiasmaShellWeapon = import('/lua/aeonweapons.lua').AIFArtilleryMiasmaShellWeapon
 local EffectTemplate = import('/lua/EffectTemplates.lua')
-local ADFCannonQuantumWeapon = AeonWeapons.ADFCannonQuantumWeapon
-local AANChronoTorpedoWeapon = import('/lua/aeonweapons.lua').AANChronoTorpedoWeapon
 
-MAS0106 = Class( ASeaUnit ) {
+
+MAS0106 = Class(ASeaUnit) {
 
     Weapons = {
 		MainGun = Class(AIFArtilleryMiasmaShellWeapon) {},
-		UpgradeGun03 = Class(ADFCannonQuantumWeapon) {
-            FxMuzzleFlash = EffectTemplate.AQuantumCannonMuzzle02
-        },
-		UpgradeGun04 = Class(ADFCannonQuantumWeapon) {
-            FxMuzzleFlash = EffectTemplate.AQuantumCannonMuzzle02
-        },
-		Torpedo01 = Class(AANChronoTorpedoWeapon) {},
-		ArtyUpgrade = Class(AIFArtilleryMiasmaShellWeapon) {},
     },
-
-   OnCreate = function(self)
+	
+    OnCreate = function(self)
 		ASeaUnit.OnCreate(self)
-		self:HideBone('Origine04_01', true)
-		self:HideBone('Turret_Center_Barrel01', true)
-		self:HideBone('Turret_Center_Barrel02', true)
-    end,
+			self:HideBone('Upgrade01', true)  
+    end,	
 	
 }
 
--- MCP/units/mes0106/mes0106_script.lua
+++ MCP/units/mes0106/mes0106_script.lua
@@ -1,33 +1,17 @@
 local TSeaUnit = import('/lua/terranunits.lua').TSeaUnit
 local TIFArtilleryWeapon = import('/lua/terranweapons.lua').TIFArtilleryWeapon
-local TDFGaussCannonWeapon = import('/lua/terranweapons.lua').TDFGaussCannonWeapon
-local TAALinkedRailgun = import('/lua/terranweapons.lua').TAALinkedRailgun
-
 local Entity = import('/lua/sim/Entity.lua').Entity
-
 
 MES0106 = Class(TSeaUnit) {
 
     Weapons = {
-		    MainGun = Class(TIFArtilleryWeapon) {},
-        UpgradeGun01 = Class(TDFGaussCannonWeapon) {},
-        UpgradeGun02 = Class(TDFGaussCannonWeapon) {},
-        UpgradeAAGun01 = Class(TAALinkedRailgun) {},
-		    UpgradeAAGun02 = Class(TAALinkedRailgun) {},
+		MainGun = Class(TIFArtilleryWeapon) {},
     },
 
-    OnCreate = function(self)
-        TSeaUnit.OnCreate(self)
-        self:HideBone('Origine01_01', true)
-        self:HideBone('Origine02_01', true)
-    end,
-	
-    OnStopBeingBuilt = function(self, builder, layer)
+    OnStopBeingBuilt = function(self,builder,layer)
         TSeaUnit.OnStopBeingBuilt(self,builder,layer)
         self.Trash:Add(CreateRotator(self, 'Spinner01', 'y', nil, 360, 0, 180))
     end,
-	
-
 }
 
 TypeClass = MES0106
