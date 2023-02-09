bf090f771a9d9277361b7ce44e1e390476e69389
SmalkiGroove <chapeau.g77@gmail.com>
2023-02-09 11:36:57 +0100

    fixes uef

--- MCP/units/mea0105/mea0105_script.lua
+++ MCP/units/mea0105/mea0105_script.lua
@@ -1,42 +1,22 @@
 local EffectTemplate = import('/lua/EffectTemplates.lua')
 local TDFRiotWeapon = import('/lua/terranweapons.lua').TDFRiotWeapon
-local TAirUnit = import('/lua/defaultunits.lua').AirTransport
+local TAirUnit = import('/lua/terranunits.lua').TAirUnit
 
 MEA0105 = Class(TAirUnit) {
 
     Weapons = {
-        Turret = Class(TDFRiotWeapon) {
+        Turret01 = Class(TDFRiotWeapon) {
             FxMuzzleFlash = EffectTemplate.TRiotGunMuzzleFxTank,
-            FxMuzzleFlashScale = 0.6,
-
-            IdleState = State(TDFRiotWeapon.IdleState) {
-                Main = function(self)
-                    self:SetAgressiveStance(0)
-                    TDFRiotWeapon.IdleState.Main(self)
-                end,
-
-                OnGotTarget = function(self)
-                    TDFRiotWeapon.OnGotTarget(self)
-                    self:SetAgressiveStance(15)
-                end,
-            },
-
-            SetAgressiveStance = function(self, stance)
-                if self.Stance ~= stance then
-                    self.Stance = stance
-                    if not self.StanceRotator then
-                        self.StanceRotator = CreateRotator(self.unit, 0, 'x', self.Stance, 10, 1)
-                    else
-                        self.StanceRotator:SetGoal(self.Stance)
-                    end
-                end
-            end,
+            FxMuzzleFlashScale = 0.75,
         },
     },
 
     OnStopBeingBuilt = function(self,builder,layer)
-        TAirUnit.OnStopBeingBuilt(self,builder,layer)                                               --XMAX, XMIN, YMAX,YMIN, ZMAX,ZMIN, TURNMULT, TURNSPEED
-        self.EngineManipulator = CreateThrustController(self, "thruster", 'Engines'):SetThrustingParam( -0.0, 0.0, -0.25, 0.25, -0.1, 0.1, 1.0, 0.25 )
+        TAirUnit.OnStopBeingBuilt(self,builder,layer)
+        self.EngineManipulators = {'Jet_Front', 'Jet_Back'}
+        for i, bone in self.EngineManipulators do                                                        --XMAX, XMIN, YMAX,YMIN, ZMAX,ZMIN, TURNMULT, TURNSPEED
+            self.EngineManipulators[i] = CreateThrustController(self, "thruster", bone):SetThrustingParam( -0.0, 0.0, -0.25, 0.25, -0.1, 0.1, 1.0, 0.25 )
+        end
     end,
 
     DestroyedOnTransport = function(self)
--- MCP/units/mrl0308/mrl0308_script.lua
+++ MCP/units/mrl0308/mrl0308_script.lua
@@ -1,77 +1,47 @@
 local CWalkingLandUnit = import('/lua/cybranunits.lua').CWalkingLandUnit
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
 local CDFElectronBolterWeapon = CybranWeaponsFile.CDFElectronBolterWeapon
-local CDFRocketIridiumWeapon = CybranWeaponsFile.CDFRocketIridiumWeapon
 local EffectUtil = import('/lua/EffectUtilities.lua')
-
 
 MRL0308 = Class(CWalkingLandUnit) {
     Weapons = {
         HeavyBolter = Class(CDFElectronBolterWeapon ) {},
-        GrenadeLauncher = Class(CDFRocketIridiumWeapon) {},
     },
     
-    AmbientExhaustBones = {
-		'Exhaust',
-    },	
-    
-    AmbientLandExhaustEffects = {
-		'/effects/emitters/dirty_exhaust_smoke_02_emit.bp',
-		'/effects/emitters/dirty_exhaust_sparks_02_emit.bp',			
-	},
-	
-	OnStopBeingBuilt = function(self, builder, layer)
-        CWalkingLandUnit.OnStopBeingBuilt(self, builder, layer)
-    end,
-    
-	CreateUnitAmbientEffect = function(self, layer)
-	    if( self.AmbientEffectThread != nil ) then
-	       self.AmbientEffectThread:Destroy()
-        end	 
-        if self.AmbientExhaustEffectsBag then
-            EffectUtil.CleanupEffectBag(self,'AmbientExhaustEffectsBag')
-        end        
-        
-        self.AmbientEffectThread = nil
-        self.AmbientExhaustEffectsBag = {} 
-	    if layer == 'Land' then
-	        self.AmbientEffectThread = self:ForkThread(self.UnitLandAmbientEffectThread)
-	    elseif layer == 'Seabed' then
-	        local army = self:GetArmy()
-			for kE, vE in self.AmbientSeabedExhaustEffects do
-				for kB, vB in self.AmbientExhaustBones do
-					table.insert( self.AmbientExhaustEffectsBag, CreateAttachedEmitter(self, vB, army, vE ))
-				end
-			end	        
-	    end          
-	end, 
-	
-	UnitLandAmbientEffectThread = function(self)
-		while not self:IsDead() do
-            local army = self:GetArmy()			
-			
-			for kE, vE in self.AmbientLandExhaustEffects do
-				for kB, vB in self.AmbientExhaustBones do
-					table.insert( self.AmbientExhaustEffectsBag, CreateAttachedEmitter(self, vB, army, vE ))
-				end
-			end
-			
-			WaitSeconds(2)
-			EffectUtil.CleanupEffectBag(self,'AmbientExhaustEffectsBag')
-
-			WaitSeconds(utilities.GetRandomFloat(1,7))
-		end		
-	end,
-
-    OnKilled = function(self, inst, type, okr)
-        -- self.Trash:Destroy()
-        -- self.Trash = TrashBag()
-        if self.AmbientExhaustEffectsBag then
-            EffectUtil.CleanupEffectBag(self,'AmbientExhaustEffectsBag')
+    OnCreate = function(self)
+        if self:GetBlueprint().General.BuildBones then
+            self:SetupBuildBones()
         end
-        CWalkingLandUnit.OnKilled(self, inst, type, okr)
+        CWalkingLandUnit.OnCreate(self)
+    end,
+
+    CreateBuildEffects = function(self, unitBeingBuilt, order)
+       EffectUtil.SpawnBuildBots(self, unitBeingBuilt, 10, self.BuildEffectsBag)
+       EffectUtil.CreateCybranBuildBeams(self, unitBeingBuilt, self:GetBlueprint().General.BuildBones.BuildEffectBones, self.BuildEffectsBag)
+    end,
+
+    OnStartBuild = function(self, unitBeingBuilt, order)
+        CWalkingLandUnit.OnStartBuild(self, unitBeingBuilt, order)
+
+        if not self.AnimationManipulator then
+            self.AnimationManipulator = CreateAnimator(self)
+            self.Trash:Add(self.AnimationManipulator)
+        end
+        self.AnimationManipulator:PlayAnim(self:GetBlueprint().Display.AnimationOpen, false):SetRate(1.5)
+        
+        self.UnitBeingBuilt = unitBeingBuilt
+        self.UnitBuildOrder = order
+        self.BuildingUnit = true
     end,
     
+    OnStopBuild = function(self, unitBeingBuilt)
+        CWalkingLandUnit.OnStopBuild(self, unitBeingBuilt)
+        if not self.AnimationManipulator then
+            self.AnimationManipulator = CreateAnimator(self)
+            self.Trash:Add(self.AnimationManipulator)
+        end
+        self.AnimationManipulator:PlayAnim(self:GetBlueprint().Display.AnimationClose, false):SetRate(1)
+    end,
 }
 
 TypeClass = MRL0308
