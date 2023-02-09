0b473910b51f81acea46fd5cb88e306a734b675d
SmalkiGroove <chapeau.g77@gmail.com>
2023-02-09 16:03:05 +0100

    fix experimental wars content

--- MCP/units/mal0204/mal0204_script.lua
+++ MCP/units/mal0204/mal0204_script.lua
@@ -1,11 +1,9 @@
 local AWalkingLandUnit = import('/lua/aeonunits.lua').AWalkingLandUnit 
 local ADFLaserLightWeapon = import('/lua/aeonweapons.lua').ADFLaserLightWeapon
-local AAAZealotMissileWeapon = import('/lua/aeonweapons.lua').AAAZealotMissileWeapon 
 
 MAL0204 = Class(AWalkingLandUnit) { 
     Weapons = { 
-        MainGun = Class(ADFLaserLightWeapon) {}, 
-        RocketBackpack = Class(AAAZealotMissileWeapon) {},
+        MainGun = Class(ADFLaserLightWeapon) {},
     }, 
 } 
 
