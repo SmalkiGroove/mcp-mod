4377b83c8d03c4259c546502d7dfdde2f477ea34
SmalkiGroove <chapeau.g77@gmail.com>
2023-12-19 23:04:42 +0100

    import 4th dimension assets

--- MCP/mod_info.lua
+++ MCP/mod_info.lua
@@ -1,6 +1,6 @@
 name = "Mixed Combat Pack"
-version = 121
-uid = "b117ef40-2b6b-4596-96c5-xCSIxMCPv121"
+version = 122
+uid = "b117ef40-2b6b-4596-96c5-xCSIxMCPv122"
 icon = "/mods/MCP/mod_icon.png"
 author = "CSI"
 copyright = "CSI"
@@ -36,11 +36,9 @@ LIST OF DEPENDENCIES (exact name) :
 * BlackOps FAF: EXUnits
 * Orbital Wars
 * Extreme Wars
-* Experimental Wars 1.96
 * Deep Blue Naval Pack
 * The Dairy Farm
 * Marlo's mod Compilation
-* 4th Dimension 2.11
 * Extreme T1 Shields
 You only need to download them, but they are not required when you start the game.
 
@@ -61,9 +59,7 @@ requiresNames = {
     ["1b6a5b64-3afe-3652-0137-8e64894b16f8"] = "Orbital Wars",
     ["2ECD8EC4-6675-11DD-8C83-853456D89593"] = "Deep Blue Naval Pack",
     ["81DE7248-XTWA-3652-0150-94B6CD7856CC"] = "Xtrem Wars",
-    ["98966d50-f62d-11de-8a39-0800200c9a66"] = "Experimental Wars",
     ["dc38db7c-419f-4b16-add1-2d4d5fa5339c"] = "Extreme T1 Shields",
     ["55D5A55I-55R5-5432-2345-555YF555A55R5"] = "The Dairy Farm",
     ["e3huey1115-1FAF2-2296-1111-15150c1s23FA"] = "Marlo's mod Compilation",
-    ["c9b309cc-0bed-4352-b9b9-ffdf81d8f37b"] = "4th Dimension",
 }
